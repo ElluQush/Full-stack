2caa7c0bd33164529d6e7c520b881894c1c12d18
Elias Majander <elias.majander@gmail.com>
2026-01-30 16:08:30 +0200

    removed cors

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
@@ -9,7 +8,6 @@ morgan.token('body', (req) => {
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
